825524686fe54e4878ef0148bbdc6d2153256d84
razmadze467-arch <razmadze467@gmail.com>
2026-09-20 13:06:41 +0400

    Place moving cars visibly over product bottles

--- js/home-max-design.js
+++ js/home-max-design.js
@@ -1070,7 +1070,7 @@
           '<i></i>' +
         '</div>';
 
-      const visual = card.querySelector('.product-visual');\n      (visual || card).appendChild(wrap);
+      const visual = card.querySelector('.product-visual');\n      card.appendChild(wrap);
     };
 
     document
