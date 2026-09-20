f3f2253fe99bd70de0875083c1ab45ee9b87e0cb
razmadze467-arch <razmadze467@gmail.com>
2026-09-20 12:25:58 +0400

    Refresh actual product card car design script

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function installBottomUI(){
 function forceMaxHomeDesign(){
   const isHome=/(^|\/)index\.html$/.test(location.pathname)||location.pathname==="/"||location.pathname==="";
   if(!isHome||document.getElementById("ecomaxMaxDesignJs"))return;
-  const s=document.createElement("script");s.id="ecomaxMaxDesignJs";s.src="js/home-max-design.js?v=20260916-3";s.defer=false;document.head.appendChild(s);
+  const s=document.createElement("script");s.id="ecomaxMaxDesignJs";s.src="js/home-max-design.js?v=20260920-4";s.defer=false;document.head.appendChild(s);
 }
 
 function init(){
